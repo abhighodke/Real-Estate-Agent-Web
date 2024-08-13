e06bdd46b466dcb1313c0f86438fd10827b84025
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-13 23:22:58 +0530

    Issues Fixed

--- Luxury Real Estate website/src/components/Contact/Contact.jsx
+++ Luxury Real Estate website/src/components/Contact/Contact.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import emailjs from 'emailjs-com';
 import backgroundImage from '../../assets/downtown.jpg'; // Ensure the path is correct
 
 const Contact = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   const [formData, setFormData] = useState({
     name: '',
     email: '',
